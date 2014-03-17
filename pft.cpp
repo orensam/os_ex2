e07aae69ea200ce9c03ec7c31301f94c81d77a04
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-03-17 19:17:10 +0200

    WORKS!

--- pft.cpp
+++ pft.cpp
@@ -7,12 +7,14 @@
 
 #include <sys/time.h>
 #include <string>
-#include "pft.h"
 #include <algorithm>
 #include <unistd.h>
 #include <limits.h>
-#include <sstream>
 #include <iostream>
+#include <queue>
+#include <exception>
+
+#include "pft.h"
 
 // Parallelism level
 static int para_level;
@@ -27,69 +29,155 @@ static const char* FILE_CMD = "file";
 static const char* FILE_FLAG_FLUSH = "-n";
 static const char* FILE_FLAG_STDIN = "-f-";
 
-// Process chuck size
-const int CHUNK_SIZE = 50;
+static const std::string ERROR_STR = " error: ";
+static const std::string FUNC_INIT = "pft_init";
+static const std::string FUNC_GET_STATS = "pft_get_stats";
+static const std::string FUNC_FIND_TYPES = "pft_find_types";
+static const std::string FUNC_SET_PARA = "setParallelismLevel";
+static const std::string FUNC_DONE = "pft_done";
 
-// Children pids array
-//std::vector<pid_t> children;
+static const std::string ERROR_BAD_ALLOC = "Memory allocation error";
+static const std::string ERROR_FORK = "Fork error";
+static const std::string ERROR_DUP = "Dup error";
+static const std::string ERROR_CLOSE = "Close error";
+static const std::string ERROR_PIPE = "Pipe error";
+static const std::string ERROR_EXECL = "Execl error";
+static const std::string ERROR_N_PARA = "Invalid parallelism level";
+static const std::string ERROR_NULLPTR = "Null pointer exception";
+static const std::string ERROR_READ = "Pipe read error";
+static const std::string ERROR_WRITE = "Pipe write error";
+
+static const char NEWLINE = '\n';
+
+
+// Return values
+static const int CODE_SUCCESS = 0;
+static const int CODE_FAIL = -1;
+
+// Process chuck size
+const int DEFAULT_CHUNK_SIZE = 50;
 
 // Parent <-> Children communication pipes
-int** outPipes; // Parent writes to children
-int** inPipes; // Parent reads from children
+//std::vector<int*> outPipes; // Parent writes to children
+//std::vector<int*> inPipes; // Parent reads from children
+int** outPipes = NULL; // Parent writes to children
+int** inPipes = NULL; // Parent reads from children
 
 // Stats
 int total_time;
 int total_file_num;
 
 
+
+/**
+ * Returns the file descriptor for child #child_num to read from parent
+ */
 int FDReadFromParent(int child_num)
 {
 	return outPipes[child_num][0];
 }
 
+/**
+ * Returns the file descriptor for parent to write to child #child_num
+ */
 int FDWriteToChild(int child_num)
 {
 	return outPipes[child_num][1];
 }
 
+/**
+ * Returns the file descriptor for parent to read from child #child_num
+ */
 int FDReadFromChild(int child_num)
 {
 	return inPipes[child_num][0];
 }
 
+/**
+ * Returns the file descriptor for child #child_num to write to parent
+ */
 int FDWriteToParent(int child_num)
 {
 	return inPipes[child_num][1];
 }
 
+void setError(const std::string& func_name, const std::string& error)
+{
+	last_error = func_name + ERROR_STR + error;
+}
+
 /**
  * Creates para_level pipes for reading and para_level pipes for writing.
- * Saved into inPipes and outPipes respectively
+ * Saved into inPipes and outPipes respectively.
  */
 int createPipes()
 {
 	inPipes = new int*[para_level];
 	outPipes = new int*[para_level];
 
+	if (!inPipes || !outPipes)
+	{
+		throw ERROR_BAD_ALLOC;
+	}
+
 	for (int child = 0; child < para_level; ++child)
 	{
-		// HANDLE ERRORS!
+		inPipes[child] = NULL;
+		outPipes[child] = NULL;
+	}
+
+	for (int child = 0; child < para_level; ++child)
+	{
+		// alloc error
 		inPipes[child] = new int[2];
 		outPipes[child] = new int[2];
-		pipe(inPipes[child]);
-		pipe(outPipes[child]);
+
+		if (!inPipes[child] || !outPipes[child])
+		{
+			throw ERROR_BAD_ALLOC;
+		}
+
+		// pipe error
+		if ( pipe(inPipes[child]) < 0 || pipe(outPipes[child]) < 0)
+		{
+			throw ERROR_PIPE;
+		}
 	}
 	pipes_inited = true;
-	return 0;
+	return CODE_SUCCESS;
 }
 
-
+int killChildren()
+{
+	if (pipes_inited)
+	{
+		for(int child = 0; child < para_level; ++child)
+		{
+			if (close(FDReadFromChild(child)) < 0 || close(FDWriteToChild(child)) < 0)
+			{
+				throw ERROR_CLOSE;
+			}
+			delete[] inPipes[child];
+			delete[] outPipes[child];
+		}
+		delete[] inPipes;
+		delete[] outPipes;
+	}
+	pipes_inited = false;
+	return CODE_SUCCESS;
+}
 
 int spawnChildren()
 {
-	createPipes();
-	pid_t parent_pid = getpid();
-	pid_t pid;
+	try
+	{
+		createPipes();
+	}
+	catch (char const* str)
+	{
+		killChildren();
+		throw;
+	}
 
 	for(int child = 0; child < para_level; ++child)
 	{
@@ -97,31 +185,39 @@ int spawnChildren()
 
 		if(pid < 0)
 		{
-			//ERROR
+			throw ERROR_FORK;
 		}
 
 		else if (pid == 0)
 		{
-			std::cout << "I am a spawned child! my pid is:" << getpid() << std::endl;
-			dup2(FDReadFromParent(child), STDIN_FILENO);
-			dup2(FDWriteToParent(child), STDOUT_FILENO);
+			//			std::cout << "I am a spawned child! my pid is:" << getpid() << std::endl;
+			if(dup2(FDReadFromParent(child), STDIN_FILENO) < 0 ||
+			   dup2(FDWriteToParent(child), STDOUT_FILENO) < 0)
+			{
+				throw ERROR_DUP;
+			}
 
-			close(FDReadFromChild(child));
-			close(FDWriteToParent(child));
-			close(FDReadFromParent(child));
-			close(FDWriteToChild(child));
+			if(close(FDReadFromChild(child)) < 0 || close(FDWriteToParent(child)) < 0 ||
+			   close(FDReadFromParent(child)) < 0 || close(FDWriteToChild(child)) < 0 )
+			{
+				throw ERROR_CLOSE;
+			}
 
 			int res = execl(FILE_CMD_PATH, FILE_CMD, FILE_FLAG_FLUSH, FILE_FLAG_STDIN, NULL);
-			std::cout << "After execl, result: " << res << std::endl;
-			// ERROR
+			if(res < 0)
+			{
+				throw ERROR_EXECL;
+			}
 		}
 		else
 		{
-			close(FDWriteToParent(child));
-			close(FDReadFromParent(child));
-			//ERROR
+			if(close(FDWriteToParent(child)) < 0 || close(FDReadFromParent(child)) < 0)
+			{
+				throw ERROR_CLOSE;
+			}
 		}
 	}
+	return CODE_SUCCESS;
 }
 
 /*
@@ -136,29 +232,17 @@ Return value:
  */
 int pft_init(int n)
 {
-	std::cout << "Start init" << std::endl;
 	pft_clear_stats();
-	setParallelismLevel(n);
-	std::cout << "End init" << std::endl;
-}
-
-
-int killChildren()
-{
-	if (pipes_inited)
+	try
 	{
-		for(int child = 0; child < para_level; ++child)
-		{
-			close(FDReadFromChild(child));
-			close(FDWriteToChild(child));
-			delete[] inPipes[child];
-			delete[] outPipes[child];
-		}
-		delete[] inPipes;
-		delete[] outPipes;
+		setParallelismLevel(n);
 	}
-	pipes_inited = false;
-	return 0;
+	catch (char const* str)
+	{
+		setError(FUNC_INIT, str);
+		return CODE_FAIL;
+	}
+	return CODE_SUCCESS;
 }
 
 
@@ -175,7 +259,17 @@ Return value:
 
 int pft_done()
 {
-	return killChildren();
+	try
+
+	{
+		return killChildren();
+	}
+	catch (const char* str)
+	{
+		setError(FUNC_DONE, str);
+		return CODE_FAIL;
+	}
+	return CODE_SUCCESS;
 }
 
 
@@ -193,11 +287,20 @@ int setParallelismLevel(int n)
 {
 	if(n <= 0)
 	{
-		//ERROR
+		setError(FUNC_SET_PARA, ERROR_N_PARA);
+		throw ERROR_N_PARA;
 	}
-	killChildren();
-	para_level = n;
-	spawnChildren();
+	try
+	{
+		killChildren();
+		para_level = n;
+		spawnChildren();
+	}
+	catch (char const* str)
+	{
+		setError(FUNC_SET_PARA, str);
+	}
+	return CODE_SUCCESS;
 }
 
 /*
@@ -208,7 +311,10 @@ int setParallelismLevel(int n)
 
 const std::string pft_get_error()
 {
+	return last_error;
 }
+
+
 
 /*
 This method initialize the given pft_stats_struct with the current statistics collected by the pft library.
@@ -233,11 +339,12 @@ int pft_get_stats(pft_stats_struct* statistic)
 {
 	if (!statistic)
 	{
-		// ERROR
+		setError(FUNC_GET_STATS, ERROR_NULLPTR);
+		return CODE_FAIL;
 	}
 	statistic->time_sec = total_time;
 	statistic->file_num = total_file_num;
-	return 0;
+	return CODE_SUCCESS;
 }
 
 /*
@@ -252,35 +359,26 @@ void pft_clear_stats()
 
 
 
-std::pair<fd_set, fd_set> getReadWriteFDs()
+fd_set getReadFDs()
 {
 	fd_set reads;
-	fd_set writes;
 	FD_ZERO(&reads);
-	FD_ZERO(&writes);
 	for(int child = 0; child < para_level; child++)
 	{
 		// Add parent read+write ends to sets
 		FD_SET(FDReadFromChild(child), &reads);
-		FD_SET(FDWriteToChild(child), &writes);
 	}
-	std::pair<fd_set, fd_set> fds = std::make_pair(reads, writes);
-	return fds;
+	return reads;
 }
 
 std::string readAllFromFD(int fd)
 {
-	std::cout << "BEGIN read all from fd " << std::endl;
-	std::string str;
-	int bytes;
 	char temp_buff[PIPE_BUF] = "";
-	while ((bytes = read(fd, temp_buff, PIPE_BUF)) > 0)
+	if (read(fd, temp_buff, PIPE_BUF) < 0)
 	{
-		std::cout << "IN WHILE read all from fd. tmp_buff is: " << temp_buff << std::endl;
-		str.append(temp_buff);
-		std::cout << "END WHILE BODY" << std::endl;
+		throw ERROR_READ;
 	}
-	std::cout << "END read all from fd " << std::endl;
+	std::string str = std::string(temp_buff);
 	return str;
 }
 
@@ -297,6 +395,7 @@ int getMaxFD()
 	}
 	return max_fd;
 }
+
 /*
 This function uses ‘file’ to calculate the type of each file in the given vector using n parallelism level.
 It gets a vector contains the name of the files to check (file_names_vec) and an empty vector (types_vec).
@@ -315,149 +414,147 @@ Return value:
  */
 int pft_find_types(std::vector<std::string>& file_names_vec, std::vector<std::string>& types_vec)
 {
-	std::cout << "Starting findTypes" << std::endl;
 	int total_files = file_names_vec.size();
-	types_vec = std::vector<std::string>(para_level);
+	types_vec = std::vector<std::string>(total_files, "");
+	int to_write = 0;
+	int send_files_n = std::min(DEFAULT_CHUNK_SIZE, total_files/para_level);
 
 	if(total_files < para_level)
 	{
 		setParallelismLevel(total_files);
 	}
 
-	int remaining_files = total_files;
-	int last_sent = 0;
-	std::vector< std::pair<int, int> > positions(para_level, std::make_pair(0,0));
+	std::vector< std::queue<int> > positions(para_level);
+	int remaining_read_files = total_files;
+	int max_fd = getMaxFD()+1;
+	fd_set reads = getReadFDs();
 
-	std::pair<fd_set, fd_set> fds = getReadWriteFDs();
-	fd_set reads = fds.first;
-	fd_set writes = fds.second;
-//	for(int child = 0; child < para_level; ++child)
-//	{
-//		int read_fd = FDReadFromChild(child);
-//		int write_fd = FDWriteToChild(child);
-//		if(FD_ISSET(read_fd, &reads))
-//		{
-//			std::cout << "Read FD: " << read_fd << std::endl;
-//		}
-//
-//		if(FD_ISSET(write_fd, &writes))
-//		{
-//			std::cout << "Write FD: " << write_fd << std::endl;
-//		}
-//	}
-
-	while(remaining_files != 0)
+	while(remaining_read_files > 0)
 	{
-		std::cout << "In while. Remaining files: " << remaining_files << std::endl;
-		fd_set ready_reads = reads;
-		fd_set ready_writes =  writes;
-
-
-		int n_ready_writes = para_level;
-
-
-		// Writing loop
-		for(int child = 0; child < para_level; ++child)
+		// Write to children
+		for(int child = 0; child < para_level && to_write < total_files; ++child)
 		{
-			int write_fd = FDWriteToChild(child);
-			if(FD_ISSET(write_fd, &ready_writes))
+			if(positions[child].empty())
 			{
-				std::cout << "Handling writing to child " << child << std::endl;
-				//send min(CHUNK_ZISE,remainig_files/ready) files
-
-				int send_files_n = std::min(CHUNK_SIZE, remaining_files/n_ready_writes);
-				positions[child].first = last_sent;
-				int j;
-				std::string filenames="";
-				for(j = positions[child].first; j < positions[child].first + send_files_n; j++)
+				std::cout << "CAN WRITE (empty queue) to child " << child << ". to_write: " << to_write << std::endl;
+				std::string filenames = "";
+				for (int i = 0; i < send_files_n && to_write < total_files; ++i, ++to_write)
 				{
-					filenames += file_names_vec[j] + "\n";
+					filenames += file_names_vec[to_write] + NEWLINE;
+					positions[child].push(to_write);
+					std::cout << "child: " << child << " index: " << to_write << std::endl;
 				}
+
+				int write_fd = FDWriteToChild(child);
 				std::cout << "Writing the string: '" << filenames << "' to child " << child << std::endl;
-				int written = write(write_fd, filenames.c_str(), filenames.size()+1);
-				if (!written)
+				int written = write(write_fd, filenames.c_str(), filenames.size());
+				std::cout << "Wrote " << written << " bytes to child " << child << std::endl;
+				if (written < 0)
 				{
-					//ERROR
+					setError(FUNC_FIND_TYPES, ERROR_WRITE);
+					return CODE_FAIL;
 				}
-				last_sent = j;
 			}
 		}
 
-		std::cout << "Before read SELECT"  << std::endl;
-		select(getMaxFD()+1, &ready_reads, NULL, NULL, NULL); //add timeout?
-		std::cout << "After read SELECT"  << std::endl;
 
-		// Reading loop
+		fd_set ready_reads(reads);
+		select(max_fd, &ready_reads, NULL, NULL, NULL);
+
+		// Read from children
 		for(int child = 0; child < para_level; ++child)
 		{
+			std::cout << "Handling read from child " << child << std::endl;
 			int read_fd = FDReadFromChild(child);
-			if(FD_ISSET(read_fd, &ready_reads))
+			if(remaining_read_files > 0 && FD_ISSET(read_fd, &ready_reads))
 			{
-				std::cout << "Handling read from child " << child << std::endl;
-				std::istringstream output(readAllFromFD(read_fd));
-
-				std::string line;
-				int startPos = positions[child].first;
-				while (std::getline(output, line))
+				std::string output;
+				try
 				{
-					std::cout << "in getline while" << std::endl;
-					types_vec.insert(types_vec.begin()+startPos, line);
-					startPos++;
-					remaining_files--;
+					output = readAllFromFD(read_fd);
+				}
+				catch (const char* str)
+				{
+					setError(FUNC_FIND_TYPES, str);
+					return CODE_FAIL;
+				}
+
+				std::cout << "Read from child " << child << " output: "<< output << std::endl;
+				int pos = 0;
+				while ((pos = output.find(NEWLINE)) != -1 && remaining_read_files > 0)
+				{
+					std::cout << "read child: " << child << ", index of newline: " << pos << ", file_vec index: " << positions[child].front() << " Got string: " << output.substr(0, pos) << std::endl;
+					types_vec[positions[child].front()].append(output.substr(0, pos));
+					positions[child].pop();
+					output = output.substr(pos + 1);
+					remaining_read_files--;
+				}
+				if (!positions[child].empty())
+				{
+					types_vec[positions[child].front()] += output;
 				}
 			}
 		}
-
-		std::cout << "After read loop" << std::endl;
-
-
-
-//		std::cout << "before write select" << std::endl;
-//		n_ready_writes = select(para_level, NULL, &ready_writes, NULL, NULL); //add timeout?
-//		std::cout << "After write select. n_ready_writes: " << n_ready_writes << std::endl;
 	}
-	std::cout << "After first SELECT"  << std::endl;
-
-	return 0;
+	return CODE_SUCCESS;
 }
+/////////////////////////////////////
+/////////////////////////////////////
 
 
-int testPipe()
-{
-	int pipefd[2];
-	pipe(pipefd);
-	fd_set fds;
-	FD_ZERO(&fds);
-	FD_SET(pipefd[0], &fds);
 
-	write(pipefd[1], "hello", 6);
 
-	std::cout << "TEST: Before SELECT"  << std::endl;
-	select(pipefd[0]+1, &fds, NULL, NULL, NULL); //add timeout?
-	std::cout << "TEST: After SELECT"  << std::endl;
 
-	return 0;
-}
+
+
 
 void printVector(std::vector<std::string>& vec)
 {
+	std::cout << "\n\n OUTPUT VECTOR:" << std::endl;
 	for (unsigned int i=0; i<vec.size(); ++i)
 	{
 		std::cout << vec[i] << std::endl;
 	}
 }
+
+
+
 int main()
 {
-//	testPipe();
-	pft_init(3);
-//	sleep(1);
+	pft_init(200);
+	sleep(1);
 	std::vector<std::string> file_names_vec;
 	std::vector<std::string> types_vec;
-	file_names_vec.push_back("test1.pdf");
-	file_names_vec.push_back("test2.tar");
-	file_names_vec.push_back("test3.zip");
+
+	for(int i=0; i<500; ++i)
+	{
+		file_names_vec.push_back("test1.pdf");
+		file_names_vec.push_back("test2.tar");
+		file_names_vec.push_back("test3.zip");
+	}
+
+//	file_names_vec.push_back("file1");
+//	file_names_vec.push_back("file2");
+//	file_names_vec.push_back("file3");
+//	file_names_vec.push_back("file4");
+//	file_names_vec.push_back("file5");
+//	file_names_vec.push_back("file6");
+//	file_names_vec.push_back("file7");
+//	file_names_vec.push_back("file8");
+//	file_names_vec.push_back("file9");
+//	file_names_vec.push_back("file1");
+//	file_names_vec.push_back("file2");
+//	file_names_vec.push_back("file3");
+//	file_names_vec.push_back("file4");
+//	file_names_vec.push_back("file5");
+//	file_names_vec.push_back("file6");
+//	file_names_vec.push_back("file7");
+//	file_names_vec.push_back("file8");
+//	file_names_vec.push_back("file9");
+
 	pft_find_types(file_names_vec, types_vec);
 	printVector(types_vec);
+	pft_done();
 }
 
 
